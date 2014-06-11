1b5053b421399b43b61128c504aa36ffcc9c89e8
bodiya <brian@bodiya.com>
2014-06-11 19:36:11 -0400

    fixed pull_up resistor

--- class3/rotary_encoder/Pi-Etch-a-Sketch-interrupt.py
+++ class3/rotary_encoder/Pi-Etch-a-Sketch-interrupt.py
@@ -9,7 +9,7 @@ import threading
 from Tkinter import *
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(23,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(24,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(25,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
