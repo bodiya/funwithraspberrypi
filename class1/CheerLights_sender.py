3c19ed622e3816fba4a9430e6cb30d7245a02b23
Tom Amlicke <tamlicke@gmail.com>
2014-06-17 19:29:41 -0400

    add simple picamera program

--- class1/CheerLights_sender.py
+++ class1/CheerLights_sender.py
@@ -6,10 +6,10 @@
 #
 import sys
 from twython import Twython
-API_KEY = '***************YOUR DATA*****************'
-API_SECRET = '***************YOUR DATA*****************'
-ACCESS_TOKEN = '***************YOUR DATA*****************'
-ACCESS_TOKEN_SECRET = '***************YOUR DATA*****************'
+API_KEY = 'TUpqM3v2o7WREzQNi9ajQ7czt'
+API_SECRET = 'Bq1WgztByKa51e20BzyMrF3bWkLuUr3EhBfTXruzgXMDrJViCS'
+ACCESS_TOKEN ='2569910537-rGRuoO7S77A4PbnsLDhxPf91qCXcxjF3nNkdLFc'
+ACCESS_TOKEN_SECRET = 'i6jZ3qDjd438bB7p6urCutaZdNc4m9MO5x6BNMtCmMD2c'
 
 api = Twython(API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET) 
 
---
+++ class4/picamera/simple.py
@@ -0,0 +1,20 @@
+#Fun with Raspberry Pi by Brian Bodiya & Tom Amlicke is licensed under a
+#Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+#Based on a work at https://github.com/bodiya/funwithraspberrypi.
+#
+#Copyright Brian Bodiya & Tom Amlicke, 2014
+#
+
+import time
+import picamera
+
+camera = picamera.PiCamera()
+
+try:
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture('photo.jpg')
+    camera.stop_preview()
+finally:
+    camera.close()
+
