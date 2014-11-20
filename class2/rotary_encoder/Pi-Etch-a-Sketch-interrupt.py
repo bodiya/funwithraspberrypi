18b05ed7c657206cd3faa4f24be01f707bddd272
Brian Bodiya <brian@bodiya.com>
2014-11-19 21:27:14 -0500

    moved some rotary specific code into rotary.py

--- class2/rotary_encoder/Pi-Etch-a-Sketch-interrupt.py
+++ class2/rotary_encoder/Pi-Etch-a-Sketch-interrupt.py
@@ -7,6 +7,7 @@
 import RPi.GPIO as GPIO
 import threading
 from Tkinter import *
+from rotary import *
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
@@ -27,17 +28,11 @@ line_length = 5
 last_wheel_status = 0
 wheel_status_lock = threading.Lock()
 move_lock = threading.Lock()
-bounce_count = 0
 vertical = False
 right_move = 0b10110100
 left_move  = 0b01111000
 
 #wheel functions
-
-#bit masks to *or* when a pin becomes active, or *and* when a pin
-#becomes inactive
-pin_masks = {24: [0b01,0b10],
-             25: [0b10,0b01]}
 
 def channel_active(channel):
   wheel_status_lock.acquire()
@@ -46,38 +41,27 @@ def channel_active(channel):
   #when using GPIO.BOTH, you can't see which edge you're on,
   #so you must read the pin again
   current_status = GPIO.input(channel)
-  last_status = (last_wheel_status & 0b00000011)
+  last_status = get_last_status(last_wheel_status)
   #print "Channel %i active: status(%i),last_status(%i)" % (channel,current_status,last_status)
   
-  #determine the new position using the last reading of the other pin,
-  #plus the current reading
-  if current_status:
-    new_status = last_status | pin_masks[channel][0]
-  else:
-    new_status = last_status & pin_masks[channel][1]
+  new_status = get_new_status_threaded(current_status, last_status, channel)
   
-  #we're bound to encounter some bounce, make sure the new reading
-  #is actually new
+  #even with bouncetime=1, we might encounter bounce, better safe than sorry
   if last_status == new_status:
-    global bounce_count
-    bounce_count += 1
     wheel_status_lock.release()
     return
   
   #update the last wheel status
-  last_wheel_status = new_status | ((last_wheel_status << 2) & 0b0011111111)
-  new_status = last_wheel_status
+  last_wheel_status = get_new_wheel_status(last_wheel_status, new_status)
 
   #release the lock on last_wheel_status, then execute the move
   wheel_status_lock.release()
   execute_move(last_wheel_status,vertical)
 
 def button_pressed(channel):
-  global vertical, bounce_count
+  global vertical
   print "Button pressed!"
-  print "%i bounces since last press" % bounce_count
   vertical ^= True
-  bounce_count = 0
 
 def execute_move(last_status,vertical):
   right = (last_status == right_move)
--- class2/rotary_encoder/rotary.py
+++ class2/rotary_encoder/rotary.py
@@ -6,18 +6,20 @@
 #
 import RPi.GPIO as GPIO
 
+####################
+#for first exercise
+####################
+
 def read_wheel(last_wheel_status):
-  left_status = GPIO.input(24) 
-  right_status = GPIO.input(25)
-  new_status = left_status | right_status << 1
-  last_status = (last_wheel_status & 0b00000011)
+  new_status = get_new_status()
+  last_status = get_last_status(last_wheel_status)
   if new_status != last_status:
     #print "New status: %i" % (new_status)
-    last_wheel_status = new_status | ((last_wheel_status << 2) & 0b0011111111)
+    last_wheel_status = get_new_wheel_status(last_wheel_status, new_status)
   return last_wheel_status
 
 def read_button(last_button_status,vertical):
-  button_status = GPIO.input(23) 
+  button_status = GPIO.input(23)
   if button_status != last_button_status:
     last_button_status = button_status
     if button_status == 1:
@@ -25,3 +27,28 @@ def read_button(last_button_status,vertical):
       vertical ^= True
   return (vertical,last_button_status)
 
+def get_last_status(last_wheel_status):
+  return last_wheel_status & 0b00000011
+
+def get_new_wheel_status(last_wheel_status, new_status):
+  return new_status | ((last_wheel_status << 2) & 0b0011111111)
+
+def get_new_status():
+  return GPIO.input(24) | GPIO.input(25) << 1
+
+####################
+#for second exercise
+####################
+
+#bit masks to *or* when a pin becomes active, or *and* when a pin
+#becomes inactive
+pin_masks = {24: [0b01,0b10],
+             25: [0b10,0b01]}
+
+def get_new_status_threaded(current_status, last_status, channel):
+  #determine the new position using the last reading of the other pin,
+  #plus the current reading
+  if current_status:
+    return last_status | pin_masks[channel][0]
+  else:
+    return last_status & pin_masks[channel][1]
